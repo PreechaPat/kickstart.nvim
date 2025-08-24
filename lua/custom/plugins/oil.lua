9019187c984fae8ec6760d9f2dddea259afe6de5
Preecha Patumcharoenpol <yumyai@gmail.com>
2025-08-24 17:22:59 +0700

    feature: Using oil instead. Also add debug function

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,39 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {
+    default_file_explorer = true,
+    columns = {
+      'icon',
+      'size',
+    },
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      padding = 2,
+      max_width = 100,
+      max_height = 30,
+      border = 'rounded',
+      win_options = {
+        winblend = 0,
+      },
+    },
+    use_default_keymaps = true,
+  },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  -- keymaps go here
+  keys = {
+    {
+      '<leader>o',
+      function()
+        require('oil').open_float()
+      end,
+      desc = 'Open Oil in floating window',
+    },
+    {
+      '-',
+      '<CMD>Oil<CR>',
+      desc = 'Open parent directory in Oil',
+    },
+  },
+}
