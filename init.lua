0751282127c4363893eb092effaa79fcf7946341
Preecha Patumcharoenpol <yumyai@gmail.com>
2025-09-12 08:06:55 +0700

    Change shortcut

--- init.lua
+++ init.lua
@@ -346,6 +346,7 @@ require('lazy').setup({
       spec = {
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
+        { '<leader>e', group = '[E]xternal tool' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
     },
